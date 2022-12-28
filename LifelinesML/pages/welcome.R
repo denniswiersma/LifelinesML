91a1ddc2e1b013955aeb91cc46baf18e9c3d0462
denniswiersma <dennis.wiersma@icloud.com>
2022-12-28 14:35:30 +0100

    changed h2 to h1

--- LifelinesML/pages/welcome.R
+++ LifelinesML/pages/welcome.R
@@ -1,5 +1,5 @@
 welcome_page <- tabPanel(
        title = "Home",
-       h2("Welcome"),
+       h1("Welcome"),
        p("Welcome to this R shiny application. This application may be used to explore, analyse, and train on the Lifelines dataset.")
 )
