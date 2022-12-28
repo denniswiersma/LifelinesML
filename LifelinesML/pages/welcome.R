668feccf3e5698dedaa4a20b0000785a999bad7f
denniswiersma <dennis.wiersma@icloud.com>
2022-12-28 14:30:47 +0100

    changed whitespace

--- LifelinesML/pages/welcome.R
+++ LifelinesML/pages/welcome.R
@@ -1,5 +1,5 @@
 welcome_page <- tabPanel(
        title = "Home",
-			 h2("Welcome"),
-			 p("Welcome to this R shiny application. This application may be used to explore, analyse, and train on the Lifelines dataset."),
-			 )
+       h2("Welcome"),
+       p("Welcome to this R shiny application. This application may be used to explore, analyse, and train on the Lifelines dataset.")
+)
