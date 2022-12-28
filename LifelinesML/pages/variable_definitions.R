de810d15865eeaa6114bcaea59b8d97807c5312b
denniswiersma <dennis.wiersma@icloud.com>
2022-12-28 17:35:30 +0100

    added whitespace between text and table

--- LifelinesML/pages/variable_definitions.R
+++ LifelinesML/pages/variable_definitions.R
@@ -8,6 +8,7 @@ variable_definitions_page <- tabPanel(
     p("Here you may find definitions for all variables in the dataset. For more information, please visit the",
         style = "display:inline"),
     a("Lifelines wiki.", href = "http://wiki.lifelines.nl/doku.php", target = "_blank"),
+    HTML("<br><br><br>"),
     dataTableOutput("variable_definition_table")
     
 )
