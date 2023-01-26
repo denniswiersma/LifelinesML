8d32c07fe01fedc5be11a653290bcc164940bc35
denniswiersma <dennis.wiersma@icloud.com>
2023-01-26 11:24:48 +0100

    added user sessions

--- LifelinesML/app.R
+++ LifelinesML/app.R
@@ -8,13 +8,7 @@ library(shinyjs)
 library(ggplot2)
 library(corrplot)
 
-# Load dataset
-dataset <- read.csv(file = "Participants_Aggregated_Age.csv",
-                    header = TRUE, sep = ",", dec = ",")
-dataset$GENDER <- as.factor(dataset$GENDER)
-dataset$GROUP_SIZE_CAT <- as.factor(dataset$GROUP_SIZE_CAT)
 
-rv <- reactiveValues(dataset = dataset)
 
 # Import all files from pages folder
 files.sources = paste0("pages/", list.files("pages/"))
@@ -50,13 +44,22 @@ ui <- fluidPage(
 )
 
 ### SERVER ###
-server <- function(input, output) {
+server <- function(input, output, session) {
+
+    # Load dataset
+    dataset <- read.csv(file = "Participants_Aggregated_Age.csv",
+                        header = TRUE, sep = ",", dec = ",")
+    dataset$GENDER <- as.factor(dataset$GENDER)
+    dataset$GROUP_SIZE_CAT <- as.factor(dataset$GROUP_SIZE_CAT)
+
+    session$userData$rv <- reactiveValues(dataset = dataset)
+
     gen_statlist_item <- function() {
         # Calculate NA values
-        stat_list <- list("total_NA" = sum(sapply(rv$dataset, function(x) sum(is.na(x)))),
-                          "cols" = ncol(rv$dataset), 
-                          "rows" = nrow(rv$dataset),
-                          "datapoints" = nrow(rv$dataset) * ncol(rv$dataset))
+        stat_list <- list("total_NA" = sum(sapply(session$userData$rv$dataset, function(x) sum(is.na(x)))),
+                          "cols" = ncol(session$userData$rv$dataset),
+                          "rows" = nrow(session$userData$rv$dataset),
+                          "datapoints" = nrow(session$userData$rv$dataset) * ncol(session$userData$rv$dataset))
         stat_list
     }
     
@@ -70,7 +73,7 @@ server <- function(input, output) {
         selectInput(
             inputId = inputID,
             label = label,
-            choices = colnames(rv$dataset),
+            choices = colnames(session$userData$rv$dataset),
             multiple = multiple
         )
     }
@@ -124,10 +127,10 @@ server <- function(input, output) {
     
     # Dataset table
     output$dataset_table <-
-        renderDataTable(rv$dataset[input$dataset_table_selection])
+        renderDataTable(session$userData$rv$dataset[input$dataset_table_selection])
 
     # Missing data table
-    output$NA_table <- renderDataTable(data.frame(variable = colnames(rv$dataset), NA_Values = sapply(rv$dataset, function(x) sum(is.na(x)))))
+    output$NA_table <- renderDataTable(data.frame(variable = colnames(session$userData$rv$dataset), NA_Values = sapply(session$userData$rv$dataset, function(x) sum(is.na(x)))))
     
     ### GENERATE PLOTS ###
     
@@ -142,13 +145,13 @@ server <- function(input, output) {
     ) +
         geom_point() +
         labs(title = paste(
-            colnames(rv$dataset[input$plots_y]),
+            colnames(session$userData$rv$dataset[input$plots_y]),
             "as a function of",
-            colnames(rv$dataset[input$plots_x])
+            colnames(session$userData$rv$dataset[input$plots_x])
         )))
     
     output$boxplot <- renderPlot(
-        ggplot(rv$dataset, aes_string(
+        ggplot(session$userData$rv$dataset, aes_string(
             x = input$plots_x,
             group = input$plots_x,
             y = input$plots_y,
@@ -158,7 +161,7 @@ server <- function(input, output) {
     )
     
     output$barplot <- renderPlot(
-        ggplot(rv$dataset, aes_string(
+        ggplot(session$userData$rv$dataset, aes_string(
             x = input$plots_x,
             fill = input$plots_cols
         )) +
@@ -166,19 +169,19 @@ server <- function(input, output) {
     )
 
     output$histogram <- renderPlot(
-      ggplot(rv$dataset, aes_string(
+      ggplot(session$userData$rv$dataset, aes_string(
         x = input$trans_var
       )) + geom_bar(fill = "#D9230D")
     )
 
     output$qqplot <- renderPlot(
-      ggplot(rv$dataset, aes_string(
+      ggplot(session$userData$rv$dataset, aes_string(
         sample = input$trans_var
       )) + stat_qq() + stat_qq_line(col = "red")
     )
 
     output$correlationplot <- renderPlot(
-      corrplot(cor(sapply(rv$dataset[input$cor_var], as.integer)),
+      corrplot(cor(sapply(session$userData$rv$dataset[input$cor_var], as.integer)),
                title = "Correlation matrix",
                type = "lower",
                tl.col = "black",
@@ -192,9 +195,9 @@ server <- function(input, output) {
     observeEvent(input$md_submit, {
         new_values <- switch(
           input$md_method,
-          "Impute by mean" = rv$dataset[which(is.na(rv$dataset[input$md_var])), input$md_var] <- mean(unlist(na.omit(rv$dataset[input$md_var]))),
-          "Impute by median" = rv$dataset[which(is.na(rv$dataset[input$md_var])), input$md_var] <- median(unlist(na.omit(rv$dataset[input$md_var]))),
-          "Delete NA rows" = rv$dataset <- rv$dataset[complete.cases(rv$dataset[input$md_var]),]
+          "Impute by mean" = session$userData$rv$dataset[which(is.na(session$userData$rv$dataset[input$md_var])), input$md_var] <- mean(unlist(na.omit(session$userData$rv$dataset[input$md_var]))),
+          "Impute by median" = session$userData$rv$dataset[which(is.na(session$userData$rv$dataset[input$md_var])), input$md_var] <- median(unlist(na.omit(session$userData$rv$dataset[input$md_var]))),
+          "Delete NA rows" = session$userData$rv$dataset <- session$userData$rv$dataset[complete.cases(session$userData$rv$dataset[input$md_var]),]
         )
         new_values
 
@@ -212,8 +215,8 @@ server <- function(input, output) {
 
         new_values <- switch(
           input$std_method,
-          "Standard score" = rv$dataset[input$std_var] <- scale(rv$dataset[input$std_var]),
-          "Min-Max" = rv$dataset[input$std_var] <- min_max(rv$dataset[input$std_var]),
+          "Standard score" = session$userData$rv$dataset[input$std_var] <- scale(session$userData$rv$dataset[input$std_var]),
+          "Min-Max" = session$userData$rv$dataset[input$std_var] <- min_max(session$userData$rv$dataset[input$std_var]),
         )
         new_values
 
@@ -227,11 +230,11 @@ server <- function(input, output) {
     observeEvent(input$trans_submit, {
         new_values <- switch(
           input$trans_method,
-          "log10" = rv$dataset[input$trans_var] <- log10(rv$dataset[input$trans_var]),
-          "log2" = rv$dataset[input$trans_var] <- log2(rv$dataset[input$trans_var]),
-          "ln" = rv$dataset[input$trans_var] <- log(rv$dataset[input$trans_var]),
-          "square root"= rv$dataset[input$trans_var] <- sqrt(rv$dataset[input$trans_var]),
-          "cube root" = rv$dataset[input$trans_var] <- rv$dataset[input$trans_var]^(1/3)
+          "log10" = session$userData$rv$dataset[input$trans_var] <- log10(session$userData$rv$dataset[input$trans_var]),
+          "log2" = session$userData$rv$dataset[input$trans_var] <- log2(session$userData$rv$dataset[input$trans_var]),
+          "ln" = session$userData$rv$dataset[input$trans_var] <- log(session$userData$rv$dataset[input$trans_var]),
+          "square root"= session$userData$rv$dataset[input$trans_var] <- sqrt(session$userData$rv$dataset[input$trans_var]),
+          "cube root" = session$userData$rv$dataset[input$trans_var] <- session$userData$rv$dataset[input$trans_var]^(1/3)
         )
         new_values
 
@@ -243,7 +246,7 @@ server <- function(input, output) {
 
     # Delete row button
     observeEvent(input$del, {
-        rv$dataset <- subset(rv$dataset, select = -which(colnames(rv$dataset) == input$del_var))
+        session$userData$rv$dataset <- subset(session$userData$rv$dataset, select = -which(colnames(session$userData$rv$dataset) == input$del_var))
         # Display confirmation message
         output$del_success <- renderText("Successfully deleted data!")
         # Remove confirmation message after 1.1 seconds
@@ -252,7 +255,7 @@ server <- function(input, output) {
 
     # Reset dataset button
     observeEvent(input$reset, {
-        rv$dataset <- dataset
+        session$userData$rv$dataset <- dataset
     })
     
 }
