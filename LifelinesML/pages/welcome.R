9d640748ac2820be6a1ac98f00975a7eca259b68
denniswiersma <dennis.wiersma@icloud.com>
2023-01-26 11:35:08 +0100

    updated welcome text

--- LifelinesML/pages/welcome.R
+++ LifelinesML/pages/welcome.R
@@ -1,7 +1,11 @@
 welcome_page <- tabPanel(
        title = "Home",
        h1("Welcome"),
-       p("Welcome to this R shiny application. This application may be used to explore, analyse, and train on the Lifelines dataset."),
+       p("Welcome to this R shiny application. This application may be used to explore and analyse the Lifelines dataset."),
+       p("This shiny application is completely open source and licenced under AGPL. You can find all code related to this project on it's",
+        style = "display:inline"),
+       a("Github page.", href = "https://github.com/denniswiersma/LifelinesML", target = "_blank"),
+
        hr(),
        h2("Dataset overview"),
        p("Number of rows:"), textOutput("rows"),
